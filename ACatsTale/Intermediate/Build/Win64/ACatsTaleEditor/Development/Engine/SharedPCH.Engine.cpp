042aee5c3088cb5a75eb6a89c5b557e0bc9559de
ryan bakerman <ryanbakerman@icloud.com>
2019-01-30 02:01:15 -0500

    dialogue triggers, built level data removed from gitignore
    separated dialogue triggers into collectible/progression; built levels and removed built_data from gitignore

--- ACatsTale/Intermediate/Build/Win64/ACatsTaleEditor/Development/Engine/SharedPCH.Engine.cpp
+++ ACatsTale/Intermediate/Build/Win64/ACatsTaleEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Chris/OneDrive/Documents/GitHub/A-Cat-s-Tale/ACatsTale/Intermediate/Build/Win64/ACatsTaleEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/desti/Documents/Unreal Projects/A Cat's Tale/A-Cat-s-Tale/ACatsTale/Intermediate/Build/Win64/ACatsTaleEditor/Development/Engine/SharedPCH.Engine.h"
